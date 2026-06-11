7353bdaa15056adf82649fbb74d96602e0eb7d84
GoldenAnpu <seryios.sych@gmail.com>
2026-06-11 23:19:00 +0000

    Update Docker configuration and dependencies for mesh support

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/data-commander:1.2.28-test-build-01 .
-docker push supervisely/data-commander:1.2.28-test-build-01
+docker build -t supervisely/data-commander:1.2.28-test-build-02 .
+docker push supervisely/data-commander:1.2.28-test-build-02
